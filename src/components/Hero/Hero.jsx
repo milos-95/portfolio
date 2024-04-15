0dca9a0073c8cfbc5a3a09e96bc65109eaa41576
Milos <milos995le@gmail.com>
2024-04-15 14:44:51 +0200

    Added React Logo and spin effect

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,20 @@
 import { getImageUrl } from "../../utils";
 import styles from "./Hero.module.css";
 
+//icons
+import { FaReact } from "react-icons/fa";
+
 function Hero() {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I&apos;m Milos</h1>
         <p className={styles.description}>
-          A front-end developer with 1 year of experience using React and
-          NodeJS. <br className={styles.break}></br> With a keen eye for detail
-          and a drive for innovation, I specialize in creating dynamic web
-          solutions that blend creativity with cutting-edge technology.
+          A front-end developer with 1 year of experience using{" "}
+          <FaReact className={styles.icons} /> React.{" "}
+          <br className={styles.break}></br> With a keen eye for detail and a
+          drive for innovation, I specialize in creating dynamic web solutions
+          that blend creativity with cutting-edge technology.
         </p>
         <a href="mailto:myemail@gmail.com" className={styles.contactBtn}>
           Contact Me

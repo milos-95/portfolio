f48160822698ed439881c587637a3cef27b96152
Milos <milos995le@gmail.com>
2024-04-17 19:52:35 +0200

    Edited Projects Title

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import ProjectCard from "./ProjectCard";
 function Projects() {
   return (
     <section className={styles.container} id="projects">
-      <h2 className={styles.title}>Projects</h2>
+      <h2 className={styles.title}>Projects I&apos;ve worked on</h2>
       <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project}></ProjectCard>;

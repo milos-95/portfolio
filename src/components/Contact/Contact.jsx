50b18dff5012a1390c979717fcb3208d15b3b5dd
Milos <milos995le@gmail.com>
2024-05-04 17:19:45 +0200

    Redesigned Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,21 +8,14 @@ function Contact() {
         <h2>Contact</h2>
         <p>Feel free to reach out!</p>
       </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
+      <ul className={styles.contactInfoContainer}>
+        <li className={styles.iconsContainer}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@gmail.com">myemail@gmail.com</a>
-        </li>
-        <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="Linkedin icon"
-          />
-          <a href="https://www.linkedin.com/myname">Linkedin.com/myname</a>
-        </li>
-        <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+        </li>
+        <li className={styles.links}>
+          <a href="mailto:janicmilos95@gmail.com">janicmilos95@gmail.com</a>
+          <a href="https://www.github.com/milos-95">github.com/milos-95</a>
         </li>
       </ul>
     </div>
